4968fee88fb65ebf4075f98068a88710011b8a2c
Tyrone <tyrone.cloma0219@gmail.com>
2022-08-30 18:49:51 -0400

    added connection between rails and react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [movieData, setMovieData] = useState([])
+
+   useEffect(() => {
+    const fetchMovies = async () => {
+      const request = await fetch('http://localhost:3000/');
+      const data = await request.json()
+      // console.log(data)
+      setMovieData(data)
+    }
+    fetchMovies()
+   },[])
+
+   console.log(movieData)
+
+   return (
+      <div className='App'>
+         <header className='App-header'>
+            <h1> ATZ Cinema </h1>
+
+         </header>
+      </div>
+   );
 }
 
 export default App;
