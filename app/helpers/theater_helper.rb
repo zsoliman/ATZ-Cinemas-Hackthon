038779d552e85001c3d5ddd22d2d51a9b60994d3
andor <latandor@gmail.com>
2022-08-30 16:17:53 -0400

    routes and controllers added

---
+++ app/helpers/theater_helper.rb
@@ -0,0 +1,2 @@
+module TheaterHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "theater#index" 
+  get "/theater/:id", to: "theater#show"
 end
