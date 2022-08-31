83a10eab04aa911211bd29c2cfb1680a2d61d06e
Tyrone <tyrone.cloma0219@gmail.com>
2022-08-30 20:13:27 -0400

    Made skeleton structure and added routes

---
+++ client/src/Cinema.jsx
@@ -0,0 +1,12 @@
+const Cinema = ({ movieData }) => {
+   const { capacity, genre, movies } = movieData;
+
+  console.log(movieData)
+
+   return (
+      <div className='cinema-page-container'>
+         <h1>  </h1>
+      </div>
+   );
+};
+export default Cinema;
